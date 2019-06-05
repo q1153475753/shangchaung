124f32bf32a7f72e6c66beff18369f1209a25f2c
xiaowenjie <1153475753@qq.com>
2019-06-05 19:41:35 +0800

    update

--- src/main/java/com/qianfeng/shangchaung/controller/OurController.java
+++ src/main/java/com/qianfeng/shangchaung/controller/OurController.java
@@ -2,9 +2,14 @@ package com.qianfeng.shangchaung.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("our")
 public class OurController {
-
+    @RequestMapping("hello")
+    @ResponseBody
+    public  String hello(){
+        return "hello";
+    }
 }
